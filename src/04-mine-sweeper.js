bc8c0fc5091d8511ceb8aa1cfecccd7ac8b85d1e
urakend@mail.ru <urakend@mail.ru>
2021-04-11 23:12:06 +0300

    feat:4 task done

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -14,15 +14,43 @@
  *  [false, false, false]
  * ]
  *
- * The result should be following:
+ * The finalarrult should be following:
  * [
  *  [1, 2, 1],
  *  [2, 1, 1],
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const finalarr = [];
+
+  for (let i = 0; i < matrix.length; ++i) {
+    finalarr.push([]);
+
+    const beforerow = matrix[i - 1];
+    const row = matrix[i];
+    const afterrow = matrix[i + 1];
+
+    for (let j = 0; j < matrix[i].length; ++j) {
+      let counter = 0;
+      // console.log(beforerow, row, afterrow);
+      [beforerow, row, afterrow].forEach((e) => {
+        if (e) {
+          if (e[j - 1] === true) counter++;
+          if (e[j] === true) counter++;
+          if (e[j + 1] === true) counter++;
+        }
+      });
+
+      if (matrix[i][j] === true) {
+        finalarr[i].push(1);
+      } else {
+        finalarr[i].push(counter);
+      }
+    }
+  }
+
+  return finalarr;
 }
 
 module.exports = minesweeper;
