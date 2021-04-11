09ee935f232ed81909a35fc4d9a7a47fe0495caf
urakend@mail.ru <urakend@mail.ru>
2021-04-11 14:17:50 +0300

    feat:1 task done

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,19 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  const sum = [];
+  const temp = matrix;
+  for (let i = 0; i < temp.length; ++i) {
+    for (let j = 0; j < temp[i].length; ++j) {
+      if (!temp[i + 1]) break;
+      if (temp[i][j] === 0) {
+        temp[i + 1][j] = 0;
+      }
+    }
+    sum.push(temp[i].reduce((acc, e) => acc + e));
+  }
+  return sum.reduce((acc, e) => acc + e);
 }
 
 module.exports = getMatrixElementsSum;
