b637c081a0f223a94e5587818fff3f5f53302371
urakend@mail.ru <urakend@mail.ru>
2021-04-12 14:40:24 +0300

    feat:9 task done

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,17 +1,9 @@
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let final = String(n).split('').reduce((acc, e) => acc + +e, 0);
+  if (String(final).length > 1) {
+    final = getSumOfDigits(final);
+  }
+  return final;
 }
 
 module.exports = getSumOfDigits;
