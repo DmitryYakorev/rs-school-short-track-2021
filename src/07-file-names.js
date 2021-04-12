eb03b8d8f520b006aff8a1d62cc03d8ce1931c47
urakend@mail.ru <urakend@mail.ru>
2021-04-12 13:14:24 +0300

    feat:7 task done

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,26 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const final = [];
+  const count = [];
+  for (let i = 0; i < names.length; i++) {
+    let temp = names[i];
+    if (count[temp] === undefined) {
+      if (final.includes(temp)) {
+        count[temp] = 1;
+        temp += `(${count[temp]})`;
+      } else {
+        count[temp] = 0;
+      }
+      final.push(temp);
+    } else {
+      count[temp] += 1;
+      temp += `(${count[temp]})`;
+      final.push(temp);
+    }
+  }
+  return final;
 }
 
 module.exports = renameFiles;
