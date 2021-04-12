bccb3f08ae3c0314162ac3331148aaf5d130c963
urakend@mail.ru <urakend@mail.ru>
2021-04-12 15:37:05 +0300

    feat:11 task done

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (!str) return '';
+  let char = str[0];
+  let count = 1;
+  let final = '';
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    } else {
+      final += (count === 1 ? '' : count) + char;
+      char = str[i];
+      count = 1;
+    }
+    if (i === str.length - 1) {
+      final += (count === 1 ? '' : count) + char;
+    }
+  }
+  return final;
 }
 
 module.exports = encodeLine;
